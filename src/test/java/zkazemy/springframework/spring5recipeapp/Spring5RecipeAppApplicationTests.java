8b2b6ddf84500b97cbb032f0117adf41fd0c37ac
zkazemy <zahrakazemy@gmail.com>
2021-02-26 11:44:03 +0330

    recipe application

---
+++ src/test/java/zkazemy/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package zkazemy.springframework.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
