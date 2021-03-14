28d3281d7c8a627c5d8c53b69beabca7605caf44
zkazemy <zahrakazemy@gmail.com>
2021-03-14 11:00:17 +0330

    Create Ingredient closes #7

--- src/main/java/zkazemy/springframework/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/zkazemy/springframework/spring5recipeapp/controllers/IngredientController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import zkazemy.springframework.spring5recipeapp.commands.IngredientCommand;
+import zkazemy.springframework.spring5recipeapp.commands.RecipeCommand;
+import zkazemy.springframework.spring5recipeapp.commands.UnitOfMeasureCommand;
 import zkazemy.springframework.spring5recipeapp.services.IngredientService;
 import zkazemy.springframework.spring5recipeapp.services.RecipeService;
 import zkazemy.springframework.spring5recipeapp.services.UnitOfMeasureService;
@@ -25,9 +27,25 @@ public class IngredientController {
         this.unitOfMeasureService = unitOfMeasureService;
     }
 
+
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model) {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        if (recipeCommand == null) {
+            throw new RuntimeException("recipe not found!");
+        }
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+        model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
+        return "recipe/ingredient/ingredientform";
+    }
+
+
     @GetMapping
     @RequestMapping("/recipe/{recipeId}/ingredients")
-    public String listIngredients(@PathVariable String recipeId, Model model){
+    public String listIngredients(@PathVariable String recipeId, Model model) {
 //        log.debug("Getting ingredient list for recipe id: " + recipeId);
 
         // use command object to avoid lazy load errors in Thymeleaf.
@@ -39,16 +57,15 @@ public class IngredientController {
     @GetMapping
     @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
     public String showRecipeIngredient(@PathVariable String recipeId,
-                                       @PathVariable String id, Model model)
-    {
-        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId),Long.valueOf(id)));
+                                       @PathVariable String id, Model model) {
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
     }
 
     @GetMapping
     @RequestMapping("recipe/{recipeId}/ingredient/{id}/update")
     public String updateRecipeIngredient(@PathVariable String recipeId,
-                                         @PathVariable String id, Model model){
+                                         @PathVariable String id, Model model) {
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
 
         model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
@@ -56,13 +73,13 @@ public class IngredientController {
     }
 
     @PostMapping("recipe/{recipeId}/ingredient")
-    public String saveOrUpdate(@ModelAttribute IngredientCommand command){
+    public String saveOrUpdate(@ModelAttribute IngredientCommand command) {
         IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command);
 
 //        log.debug("saved receipe id:" + savedCommand.getRecipeId());
 //        log.debug("saved ingredient id:" + savedCommand.getId());
 
-        return "redirect:/recipe/" + command.getRecipeId() + "/ingredient/" + command.getId() + "/show";
+        return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
     }
 
 }
